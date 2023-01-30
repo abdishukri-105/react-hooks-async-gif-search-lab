4c70bb4c2311aebbb0b4f78881e761621d5ac684
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-30 18:32:27 +0300

    format code

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,18 +2,14 @@ const GifList = ({gifs}) => {
 
   const renderGifs = gifs.map(gif => {
     return <li>
-        <img src={gif.images.original.url} alt="image" />
-         
-    </li>
+             <img src={gif.images.original.url} alt="image" />
+           </li>
   }) 
 
-    return ( 
-   
-           <ul>
-             {renderGifs}
-           </ul>
-      
-
+    return (
+        <ul>
+          {renderGifs}
+        </ul>
      );
 }
  
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -17,8 +17,6 @@ const GifListContainer = () => {
       })
   },[searchTerm] );
 
-
-
   const handleSubmit = (value) => {
     setSearchTerm(value);
   };
